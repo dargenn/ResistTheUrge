0d5c4a9cbe0a2ff3b84d9500c7f354940e2a2f00
dargenn <dargenn@o2.pl>
2016-12-04 16:45:43 +0100

    Wyswietlanie wyniku i detekcja kolizji

--- main.js
+++ main.js
@@ -1,93 +1,88 @@
-// Create our 'main' state that will contain the game
 var mainState = {
     preload: function() {
-        // Load the bird sprite
         game.load.image('bird', 'assets/bird.png');
-        // Load pipe
         game.load.image('pipe', 'assets/pipe.png');
     },
 
     create: function() {
-        // Change the background color of the game to blue
+        // Kolor tła - mozna zmienic na fajniejszy
         game.stage.backgroundColor = '#71c5cf';
 
-        // Set the physics system
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
-        // Display the bird at the position x=100 and y=245
+        // Pozycja startowa
         this.bird = game.add.sprite(100, 245, 'bird');
 
-        // Add physics to the bird
-        // Needed for: movements, gravity, collisions, etc.
+        // Dodanie fizyki - potrzebna do ruchu, grawitacji i kolizji
         game.physics.arcade.enable(this.bird);
 
-        // Add gravity to the bird to make it fall
+        // Grawitacja - jak szybko spada
         this.bird.body.gravity.y = 1000;
 
-        // Call the 'jump' function when the spacekey is hit
+        // Na wcisniecie spacji - skacz
         var spaceKey = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
 
-        // Create an empty group
+        // Pusta grupa rur
         this.pipes = game.add.group();
 
-        // Moving pipes
+        // Ruch rur
         this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+        // Wyswietlanie wyniku
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0",
+            { font: "30px Arial", fill: "#ffffff" });
     },
 
     update: function() {
-        // If the bird is out of the screen (too high or too low)
-        // Call the 'restartGame' function
+        // Restart gdy bedzie za wysoko lub za nisko
         if (this.bird.y < 0 || this.bird.y > 490)
             this.restartGame();
+
+        // Restart, gdy wykryje kolizje
+        game.physics.arcade.overlap(this.bird, this.pipes, this.restartGame, null, this);
     },
 
     jump: function() {
-        // Add a vertical velocity to the bird
+        // Jak wysoko ma skakac
         this.bird.body.velocity.y = -350;
     },
 
     restartGame: function() {
-        // Start the 'main' state, which restarts the game
         game.state.start('main');
     },
 
     addOnePipe: function(x, y) {
-        // Create a pipe at the position x and y
         var pipe = game.add.sprite(x, y, 'pipe');
-
-        // Add the pipe to our previously created group
         this.pipes.add(pipe);
-
-        // Enable physics on the pipe
         game.physics.arcade.enable(pipe);
 
-        // Add velocity to the pipe to make it move left
+        // Ruch rur w lewo
         pipe.body.velocity.x = -200;
 
-        // Automatically kill the pipe when it's no longer visible
+        // Gdy rura nie jest widoczna - usun ja
         pipe.checkWorldBounds = true;
         pipe.outOfBoundsKill = true;
     },
 
     addRowOfPipes: function() {
-        // Randomly pick a number between 1 and 5
-        // This will be the hole position
+        // Randomowa pozycja dziury w rurach - trzeba miec ktoredy przeleciec
         var hole = Math.floor(Math.random() * 5) + 1;
 
-        // Add the 6 pipes
-        // With one big hole at position 'hole' and 'hole + 1'
+        // 6 rur z jedna dziura na dwa miejsca
         for (var i = 0; i < 8; i++)
             if (i != hole && i != hole + 1)
                 this.addOnePipe(400, i * 60 + 10);
-    },
+
+        // Zinkrementuj wynik
+        this.score++;
+        this.labelScore.text = this.score;
+    }
 };
 
-// Initialize Phaser, and create a 400px by 490px game
+//Init
 var game = new Phaser.Game(400, 490);
 
-// Add the 'mainState' and call it 'main'
 game.state.add('main', mainState);
-
-// Start the state to actually start the game
 game.state.start('main');
